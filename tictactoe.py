ed2fef8e046025cbaffdb99f610635834c1da105
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-05 16:37:16 -0400

    finished display for tictactoe game from console

---
+++ tictactoe.py
@@ -0,0 +1,8 @@
+game = [[0,0,0],[0,0,0],[0,0,0]]
+
+print('   A  B  C')
+
+for count, line in enumerate(game):
+    print(count, line)
+     for item in line:
+        #item is a singular element of the nested list
