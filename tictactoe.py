0f92c7d755620fe515290fca7382d30cc4e09cd0
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-09 20:05:00 -0400

    migrating data from python practice to github because I'm changing to another device

--- tictactoe.py
+++ tictactoe.py
@@ -1,16 +1,13 @@
 game = [[0,0,0],[0,0,0],[0,0,0]]
 
 
-def game_screen(player=0, row=0, column=0):
-    if player != 0:
+def game_screen(player=0, row=0, column=0, just_display=False):
+    if not just_display:
         game[row][column] = player
     print('   0  1  2')
     for count, line in enumerate(game):
         print(count, line)
 
-game_screen()
-
-game[0][1] = 1
-
-game_screen()
+game_screen(just_display=True)
+game_screen(1, 2, 1)
 
